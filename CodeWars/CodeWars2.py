1dab40ecae59c81dd9d8d4f4db935a928b7c89a7
Egor_535 <edudov82@gmail.com>
2024-04-12 15:59:59 +0300

    commit codewars2

--- CodeWars/CodeWars2.py
+++ CodeWars/CodeWars2.py
@@ -1,4 +1,25 @@
+import re
+
 def generate_hashtag(s):
     return "".join("#" + "".join(s.strip().title().split())) if len("".join("#" + "".join(s.strip().title().split()))) <= 140 and len("".join("#" + "".join(s.strip().title().split()))) > 1 else False
 
-print(generate_hashtag(''))
+
+def parse_int(string):
+    ONES = {'zero': 0,'one': 1,'two': 2,'three': 3,'four': 4,'five': 5,'six': 6,'seven': 7,'eight': 8,'nine': 9,'ten': 10,'eleven': 11,'twelve': 12,'thirteen': 13,
+            'fourteen': 14,'fifteen': 15,'sixteen': 16,'seventeen': 17,'eighteen': 18,'nineteen': 19,'twenty': 20,'thirty': 30,'forty': 40,'fifty': 50,
+            'sixty': 60,'seventy': 70,'eighty': 80,'ninety': 90,
+              }
+    numbers = []
+    for token in string.replace('-', ' ').split(' '):
+        if token in ONES:
+            numbers.append(ONES[token])
+        elif token == 'hundred':
+            numbers[-1] *= 100
+        elif token == 'thousand':
+            numbers = [x * 1000 for x in numbers]
+        elif token == 'million':
+            numbers = [x * 1000000 for x in numbers]
+    return sum(numbers)
+
+
+print(parse_int("seven hundred thousand"))
